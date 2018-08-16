b4166e6c8624e0b34f33d9e9fe0808322716f8e2
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-16 13:55:55 -0300

    verificando dados do form

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-form',
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
   
  
+  @Input() form;
   usuario: any = {
-    
-  };
+    nome: '',
+    email: ''};
   constructor() { }
 
   ngOnInit() {
@@ -18,7 +19,6 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form){
     console.log(form.value);
-    console.log(this.usuario);
   }
 
 }
