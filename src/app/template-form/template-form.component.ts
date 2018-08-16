3a525012db22936162eb23ed9a63d625b8615b7a
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-16 12:54:07 -0300

    modulo

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -6,14 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
-
+  
+  usuario: any = {
+    nome: 'Loiane',
+    email: 'loiane@email.com'
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
   onSubmit(form){
-    console.log(form);
+    console.log(form.value);
+    console.log(this.usuario);
   }
 
 }
